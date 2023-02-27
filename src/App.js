ee384f4e4834a09d4713de777ac9268bead0f32d
Mehadyhasanakash <mehadyhasan1000@gmail.com>
2023-02-27 11:16:54 +0600

    file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import Home from "./Page/Home/Home";
-import Navber from "./Page/Navber/Navber";
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import"./style/main.css"
 function App() {
   return (
     <div>
-      <Navber />
-      <Home />
-      <div class="container"></div>
+
+      <p>This is me how are you</p>
+
+
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-          primary: "#F22E2E",
-
-          secondary: "#F2E4DC",
-
-          accent: "#588C87",
-
-          neutral: "#F3F4F6",
-
-          "base-100": "#FFFFFF",
-
-          info: "#98A8DD",
-
-          success: "#1BBB70",
-
-          warning: "#DF7E07",
-
-          error: "#FA5C5C",
-        },
-      },
-    ],
-  },
+  
+    themes: {
+      extend:{},
+    },
+  
   plugins: [require("daisyui")],
 };
