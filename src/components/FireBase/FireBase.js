48669da50114aece161a952a9552b111b689e70d
Mehadyhasanakash <mehadyhasan1000@gmail.com>
2023-03-06 16:17:10 +0600

    fire-base

---
+++ src/components/FireBase/FireBase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWwlpJyTJn81xMjyIKuWyahYZNxrwh9f0",
+  authDomain: "blood-donner-16f30.firebaseapp.com",
+  projectId: "blood-donner-16f30",
+  storageBucket: "blood-donner-16f30.appspot.com",
+  messagingSenderId: "274622522252",
+  appId: "1:274622522252:web:7335cc9e0e7a3982a46ca9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
